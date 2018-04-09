c12fc18704e71ff60f2f222ce9dcfedc9c537e70
鈴木 賢 <dddlia3@gmail.com>
2018-04-09 18:56:55 +0900

    add test code for line-bot-sdk

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 echo "hello world"
 
+function replyTextMessage($bot, $replyToken, $text) {
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+  if (!$response->isSucceeded()) {
+    error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
+}
+
  ?>
